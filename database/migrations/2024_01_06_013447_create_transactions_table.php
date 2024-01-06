3d54f1e7f4842ad99e259b35b9759957b4df4b8f
mahardikaws <mahardikawsp@gmail.com>
2024-01-06 08:51:23 +0700

    lets talk

---
+++ database/migrations/2024_01_06_013447_create_transactions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->string('description');
+            $table->date('date');
+            $table->integer('qty');
+            $table->decimal('cost', 10, 2);
+            $table->decimal('price', 10, 2);
+            $table->decimal('total_cost', 10, 2);
+            $table->integer('qty_balance');
+            $table->decimal('value_balance', 10, 2);
+            $table->decimal('hpp', 10, 4);
+            $table->timestamp('created_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
